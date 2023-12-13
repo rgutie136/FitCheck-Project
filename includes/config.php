8387dc0909952e445fece6e78aef31d9528031a8
rgutie136 <44955605+rgutie136@users.noreply.github.com>
2023-12-13 00:27:40 +0000

    verify secrets enviroment variables working

--- includes/config.php
+++ includes/config.php
@@ -2,11 +2,11 @@
 	//Used to connect the hosted sql database to php
 
 	$server = getenv("sqlServername");
-	$dbname = getenv("sqlUser");
+	$dbUser = getenv("sqlUser");
 	$dbpwd = getenv("sqlPw");
 	$dbname = "sql5660472"; 
 
-	$conn = mysqli_connect($server, $dbname, $dbpwd, $dbname);
+	$conn = mysqli_connect($server, $dbUser, $dbpwd, $dbname);
 
 	if(!$conn) {
 		die("Connection Failed: ". mysqli_connect_error());
